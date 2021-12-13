f27b6656f7b9717465eccdf9d6e9c9b70baf331f
Jeroen <gitlab.com@hendricksen.eu>
2021-12-13 22:45:56 +0100

    Add micronaut example

---
+++ micronaut/micronaut-ws/src/main/java/com/infosupport/kc/dockeressentials/Application.java
@@ -0,0 +1,10 @@
+package com.infosupport.kc.dockeressentials;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
